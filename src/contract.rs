1a07fe4225c3c7cbe1db8b52123126dfcf1a8629
Matt <38109405+mmcguire91@users.noreply.github.com>
2022-05-26 21:34:28 -0500

    update to add counter logic

--- src/contract.rs
+++ src/contract.rs
@@ -10,12 +10,28 @@ enum StorageKey {
 
 #[near_bindgen]
 #[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
-pub struct Contract {}
+pub struct Contract {
+    pub counter: u32,
+}
 
 #[near_bindgen]
 impl Contract {
     #[init]
     pub fn new() -> Self {
-        Self {}
+        Self {
+            counter: 0,
+        }
+    }
+
+    pub fn increment_counter(mut self) {
+        self.counter +=1;
+    }
+
+    pub fn decrement_counter(mut self) {
+        self.counter -=1;
+    }
+
+    pub fn get_counter(self) -> u32 {
+        return self.counter;
     }
 }
